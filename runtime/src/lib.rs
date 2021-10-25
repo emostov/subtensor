bc00680542f04887426a89ae489b1b3dbb986951
Ala Shaabana <shaabana@gmail.com>
2021-10-25 16:28:24 -0400

    Fixed comment

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -305,8 +305,8 @@ pub type SignedExtra = (
 	frame_system::CheckGenesis<Runtime>,
 	frame_system::CheckEra<Runtime>,
 	frame_system::CheckNonce<Runtime>,
-	frame_system::CheckWeight<Runtime>,
-	/// pallet_subtensor::ChargeTransactionPayment<Runtime>
+	frame_system::CheckWeight<Runtime>
+	// pallet_subtensor::ChargeTransactionPayment<Runtime>
 );
 /// Unchecked extrinsic type as expected by this runtime.
 pub type UncheckedExtrinsic = generic::UncheckedExtrinsic<Address, Call, Signature, SignedExtra>;
