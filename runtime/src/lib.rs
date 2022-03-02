b41a01d012b045120ab412c51aa7f3eb0d887db6
Thebes <jake@bittensor.com>
2022-03-02 16:12:28 -0500

    version numebr

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -96,10 +96,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
 	// This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
 	//   the compatible custom types.
-	spec_version: 109,
-	impl_version: 6,
+	spec_version: 111,
+	impl_version: 7,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 6,
+	transaction_version: 7,
 };
 
 /// This determines the average expected block time that we are targeting.
